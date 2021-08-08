848d036cab8dd747d693909d38ec46f6204aa4ae
Shofiya Bootwala <shofiyabootwala@gmail.com>
2021-08-08 17:36:32 +0530

    States In Functional Component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 import Practice from './Components/FunctionalComponent/Practice'
+import ClassC from './Components/FunctionalComponent/ClassComponene/ClassC';
+
+import React,{Component} from 'react';
+import {useState} from 'react';
+
+
 
 function App() {
+
+  const [myName,setData]=useState("shofiya")
+  const [number,updateNumber]=useState(0);
+  const names=["Shinchan","Doremon","Ziyaad","Mamma","Chota Bheem","Rocky","XYZ"];
+
+  function Clicked(){
+    let n=Math.floor((Math.random())*names.length);
+    console.log(n);
+    setData(names[n]);
+  }
+
+  
+  function update(){
+    updateNumber(number+1);
+  }
+  console.log("-------------");
   return (
     <div className="App">
       
-      <h1>Hello React!</h1>
-      <Practice />
+      <h1>Hello {myName}</h1>
+      <h2>{number}</h2>
+      {/* <Practice />
       <PracticeFunctional />
+      <ClassC />
+      <User /> */}
+      <button onClick={Clicked}>Update Name</button>
+      <button onClick={update}>Increase</button>
     </div>
   );
 }
 
-
+//Functional Component
 function PracticeFunctional(){
 
   return(
@@ -24,6 +51,21 @@ function PracticeFunctional(){
     
   </div>
   )
+}
+
+//Class component
+class User extends Component
+{
+  render()
+  {
+    return(
+      <div>
+          <p>Class component inside App.js</p>
+      </div>
+      
+    )
+  }
+ 
 }
 
 export default App;
---
+++ src/Components/FunctionalComponent/ClassComponene/ClassC.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class ClassC extends React.Component
+{
+    render()
+    {
+        return(
+            <h1>My first class component</h1>
+        )
+    }
+}
+
+export default ClassC;
